8e67b3292c452af53934770fd39cb20d9d30d86d
Dobrica Pavlinusic <dpavlin@rot13.org>
2007-10-26 14:39:42 +0000

    create custom targets in makefile (debug is first one)
    git-svn-id: https://perl-cwmp.googlecode.com/svn/trunk@115 836a5e1a-633d-0410-964b-294494ad4392

---
+++ lib/Module/Install/PRIVATE.pm
@@ -0,0 +1,38 @@
+# Dobrica Pavlinusic, <dpavlin@rot13.org> 10/26/07 14:26:36 CEST
+package Module::Install::PRIVATE;
+
+use strict;
+use warnings;
+
+use base 'Module::Install::Base';
+our $VERSION = '0.01';
+
+=head1 NAME
+
+Module::Install::PRIVATE - Module Install Support
+
+=head1 SYNOPSIS
+
+=head1 DESCRIPTION
+
+=head2 html_target
+
+=cut
+
+sub my_targets {
+	my ($self) = @_;
+
+	$self->postamble(<<"END_MAKEFILE");
+# --- $self section:
+
+debug:
+	./bin/acs.pl -d -d -d 2>&1 | tee log
+
+END_MAKEFILE
+
+	warn "added my targets\n";
+
+	return $self;
+}
+
+1;
