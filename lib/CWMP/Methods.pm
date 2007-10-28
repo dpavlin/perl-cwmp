172db50375bdd0143d5b4de649606d90057317ca
Dobrica Pavlinusic <dpavlin@rot13.org>
2007-10-28 16:57:34 +0000

    no functional changes, just cleanup of pod and re-order methods
    git-svn-id: https://perl-cwmp.googlecode.com/svn/trunk@173 836a5e1a-633d-0410-964b-294494ad4392

--- lib/CWMP/Methods.pm
+++ lib/CWMP/Methods.pm
@@ -15,11 +15,11 @@ use Data::Dump qw/dump/;
 
 CWMP::Methods - generate SOAP meesages for CPE
 
-=head2 METHODS
+=head1 METHODS
 
 =head2 new
 
-  my $response = CWMP::Methods->new({ debug => 1 });
+  my $method = CWMP::Methods->new({ debug => 1 });
 
 =cut
 
@@ -32,108 +32,16 @@ sub new {
 	return $self;
 }
 
-
-my $cwmp = [ cwmp => 'urn:dslforum-org:cwmp-1-0' ];
-my $soap = [ soap => 'http://schemas.xmlsoap.org/soap/envelope/' ];
-my $xsd  = [ xsd  => 'http://www.w3.org/2001/XMLSchema-instance' ];
-
-=head2 InformResponse
-
-  $response->InformResponse( $state );
-
-=cut
-
-sub InformResponse {
-	my ( $self, $state ) = @_;
-	$self->xml( $state, sub {
-		my ( $X, $state ) = @_;
-		$X->InformResponse( $cwmp,
-			$X->MaxEnvelopes( $cwmp, 1 )
-		);
-	});
-}
-
-=head2 GetRPCMethods
-
-  $response->GetRPCMethods( $state );
-
-=cut
-
-sub GetRPCMethods {
-	my ( $self, $state ) = @_;
-	$self->xml( $state, sub {
-		my ( $X, $state ) = @_;
-		$X->GetRPCMethods();
-	});
-};
-
-=head2 Reboot
-
-  $response->Reboot( $state );
-
-=cut
-
-sub Reboot {
-	my ( $self, $state ) = @_;
-	$self->xml( $state, sub {
-		my ( $X, $state ) = @_;
-		$X->Reboot();
-	});
-}
-
-=head2 GetParameterNames
-
-  $response->GetParameterNames( $state, $ParameterPath, $NextLevel );
-
-=cut
-
-sub GetParameterNames {
-	my ( $self, $state, $ParameterPath, $NextLevel ) = @_;
-	$ParameterPath ||= '';	# all
-	$NextLevel ||= 0;		# all
-	warn "# GetParameterNames( '$ParameterPath', $NextLevel )\n" if $self->debug;
-	$self->xml( $state, sub {
-		my ( $X, $state ) = @_;
-
-		$X->GetParameterNames( $cwmp,
-			$X->ParameterPath( $cwmp, $ParameterPath ),
-			$X->NextLevel( $cwmp, $NextLevel ),
-		);
-	});
-}
-
-=head2 GetParameterValues
-
-  $response->GetParameterValues( $state, $ParameterNames );
-
-=cut
-
-sub GetParameterValues {
-	my $self = shift;
-	my $state = shift;
-	my @ParameterNames = @_;
-	confess "need ParameterNames" unless @ParameterNames;
-	warn "# GetParameterValues", dump( @ParameterNames ), "\n" if $self->debug;
-
-	$self->xml( $state, sub {
-		my ( $X, $state ) = @_;
-
-		$X->GetParameterValues( $cwmp,
-			$X->ParameterNames( $cwmp,
-				map {
-					$X->string( $xsd, $_ )
-				} @ParameterNames
-			)
-		);
-	});
-}
-
 =head2 xml
 
 Used to implement methods which modify just body of soap message.
 For examples, see source of this module.
 
 =cut
+
+my $cwmp = [ cwmp => 'urn:dslforum-org:cwmp-1-0' ];
+my $soap = [ soap => 'http://schemas.xmlsoap.org/soap/envelope/' ];
+my $xsd  = [ xsd  => 'http://www.w3.org/2001/XMLSchema-instance' ];
 
 sub xml {
 	my $self = shift;
@@ -157,5 +65,112 @@ sub xml {
 		$X->Body( $soap, $closure->( $X, $state ) ),
 	);
 }
+
+=head1 CPE methods
+
+=head2 GetRPCMethods
+
+  $method->GetRPCMethods( $state );
+
+=cut
+
+sub GetRPCMethods {
+	my ( $self, $state ) = @_;
+	$self->xml( $state, sub {
+		my ( $X, $state ) = @_;
+		$X->GetRPCMethods();
+	});
+};
+
+=head2 SetParameterValues
+
+B<not implemented>
+
+=head2 GetParameterValues
+
+  $method->GetParameterValues( $state, $ParameterNames );
+
+=cut
+
+sub GetParameterValues {
+	my $self = shift;
+	my $state = shift;
+	my @ParameterNames = @_;
+	confess "need ParameterNames" unless @ParameterNames;
+	warn "# GetParameterValues", dump( @ParameterNames ), "\n" if $self->debug;
+
+	$self->xml( $state, sub {
+		my ( $X, $state ) = @_;
+
+		$X->GetParameterValues( $cwmp,
+			$X->ParameterNames( $cwmp,
+				map {
+					$X->string( $xsd, $_ )
+				} @ParameterNames
+			)
+		);
+	});
+}
+
+=head2 GetParameterNames
+
+  $method->GetParameterNames( $state, $ParameterPath, $NextLevel );
+
+=cut
+
+sub GetParameterNames {
+	my ( $self, $state, $ParameterPath, $NextLevel ) = @_;
+	$ParameterPath ||= '';	# all
+	$NextLevel ||= 0;		# all
+	warn "# GetParameterNames( '$ParameterPath', $NextLevel )\n" if $self->debug;
+	$self->xml( $state, sub {
+		my ( $X, $state ) = @_;
+
+		$X->GetParameterNames( $cwmp,
+			$X->ParameterPath( $cwmp, $ParameterPath ),
+			$X->NextLevel( $cwmp, $NextLevel ),
+		);
+	});
+}
+
+=head2 Reboot
+
+  $method->Reboot( $state );
+
+=cut
+
+sub Reboot {
+	my ( $self, $state ) = @_;
+	$self->xml( $state, sub {
+		my ( $X, $state ) = @_;
+		$X->Reboot();
+	});
+}
+
+
+=head1 Server methods
+
+
+=head2 InformResponse
+
+  $method->InformResponse( $state );
+
+=cut
+
+sub InformResponse {
+	my ( $self, $state ) = @_;
+	$self->xml( $state, sub {
+		my ( $X, $state ) = @_;
+		$X->InformResponse( $cwmp,
+			$X->MaxEnvelopes( $cwmp, 1 )
+		);
+	});
+}
+
+=head1 BUGS
+
+All other methods are unimplemented.
+
+=cut
 
 1;
