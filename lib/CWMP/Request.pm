397dff9ff9273c840cc59d5c2a6bde0540da7a4e
Dobrica Pavlinusic <dpavlin@rot13.org>
2007-10-30 15:46:40 +0000

    uf, fix code to actually work with documentation :-\
    git-svn-id: https://perl-cwmp.googlecode.com/svn/trunk@186 836a5e1a-633d-0410-964b-294494ad4392

--- lib/CWMP/Request.pm
+++ lib/CWMP/Request.pm
@@ -12,7 +12,138 @@ use Carp qw/confess cluck/;
 
 CWMP::Request - parse SOAP request metods
 
+=head1 CPE metods
+
+=cut
+
+my $tree = CWMP::Tree->new({ debug => 0 });
+
+our $state;	# FIXME check this!
+
+my $rules =  [
+		#_default => 'content trim',
+		x_default => sub {
+			my ($tag_name, $tag_hash, $context, $parent_data) = @_;
+			warn dump( $tag_name, $tag_hash, $context );
+		},
+		'ID' => sub {
+			my ($tag_name, $tag_hash, $context, $parent_data) = @_;
+			$state->{ID} = $tag_hash->{_content};
+		},
+
+		'DeviceId' => sub {
+			my ($tag_name, $tag_hash, $context, $parent_data) = @_;
+			foreach my $name ( keys %$tag_hash ) {
+				next if $name eq '_content';
+				my $key = $name;
+				$key =~ s/^\w+://;	# stip namespace
+				$state->{DeviceID}->{ $key } = _tag( $tag_hash, $name, '_content' );
+			}
+		},
+		'EventStruct' => sub {
+			my ($tag_name, $tag_hash, $context, $parent_data) = @_;
+			push @{ $state->{EventStruct} }, $tag_hash->{EventCode}->{_content};
+		},
+		qr/(MaxEnvelopes|CurrentTime|RetryCount)/ => sub {
+			my ($tag_name, $tag_hash, $context, $parent_data) = @_;
+			$state->{$tag_name} = $tag_hash->{_content};
+		},
+		'ParameterValueStruct' => sub {
+			my ($tag_name, $tag_hash, $context, $parent_data) = @_;
+			# Name/Value tags must be case insnesitive
+			my $value = (grep( /value/i, keys %$tag_hash ))[0];
+			$state->{Parameter}->{ _tag($tag_hash, 'Name', '_content') } = _tag($tag_hash, 'Value', '_content' );
+		},
+
+];
+
+=head2 Inform
+
+Generate InformResponse to CPE
+
+=cut
+
+push @$rules,
+	'Inform' => sub {
+		$state->{_dispatch} = 'InformResponse';		# what reponse to call
+	};
+
+=head2 GetRPCMethodsResponse
+
+=cut
+
+push @$rules,
+		qr/^(?:^\w+:)*string$/ => 'content array',
+		'MethodList' => sub {
+			my ($tag_name, $tag_hash, $context, $parent_data) = @_;
+			$state->{MethodList} = _tag( $tag_hash, 'string' );
+		};
+
+=head2 GetParameterNamesResponse
+
+=cut
+
+push @$rules,
+		'ParameterInfoStruct' => sub {
+			my ($tag_name, $tag_hash, $context, $parent_data) = @_;
+			my $name = _tag($tag_hash, 'Name', '_content');
+			my $writable = _tag($tag_hash, 'Writable', '_content' );
+
+			confess "need state" unless ( $state );	# don't remove!
+
+			# XXX dragons ahead: convert name to tree rewriting it into perl
+			my $s = '$state->{ParameterInfo}->' . $tree->name2perl( $name ) . "->{writable} = $writable;";
+			eval "$s";
+			confess "can't eval $s : $@" if ($@);
+
+			#warn "## state = dump( $state ), "\n";
+		};
+	
+=head2 Fault
+
+=cut
+
+push @$rules,
+		'Fault' => sub {
+			my ($tag_name, $tag_hash, $context, $parent_data) = @_;
+			$state->{Fault} = {
+				FaultCode => _tag( $tag_hash, 'FaultCode', '_content' ),
+				FaultString => _tag( $tag_hash, 'FaultString', '_content' ),
+			};
+			warn "FAULT: ", $state->{Fault}->{FaultCode}, " ", $state->{Fault}->{FaultString}, "\n";
+		};
+
+my $parser = XML::Rules->new(
+#	start_rules => [
+#		'^division_name,fax' => 'skip',
+#	],
+	namespaces => {
+		'http://schemas.xmlsoap.org/soap/envelope/' => 'soapenv',
+		'http://schemas.xmlsoap.org/soap/encoding/' => 'soap',
+		'http://www.w3.org/2001/XMLSchema' => 'xsd',
+		'http://www.w3.org/2001/XMLSchema-instance' => 'xsi',
+		'urn:dslforum-org:cwmp-1-0' => '',
+	},
+	rules => $rules,
+);
+
 =head1 METHODS
+
+=head2 parse
+
+  my $state = CWMP::Request->parse( "<soap>request</soap>" );
+
+=cut
+
+sub parse {
+	my $self = shift;
+
+	my $xml = shift || confess "no xml?";
+
+	$state = {};
+	$parser->parsestring( $xml );
+	return $state;
+}
 
 =head2 _tag
 
@@ -44,127 +175,5 @@ sub _tag {
 		return;
 	}
 }
-
-=head2 parse
-
-  my $state = CWMP::Request->parse( "<soap>request</soap>" );
-
-=cut
-
-sub parse {
-	my $self = shift;
-
-	my $xml = shift || confess "no xml?";
-
-	$state = {};
-	$parser->parsestring( $xml );
-	return $state;
-}
-
-=head1 CPE metods
-
-=cut
-
-our $state;	# FIXME check this!
-
-my $tree = CWMP::Tree->new({ debug => 0 });
-
-my $parser = XML::Rules->new(
-#	start_rules => [
-#		'^division_name,fax' => 'skip',
-#	],
-	namespaces => {
-		'http://schemas.xmlsoap.org/soap/envelope/' => 'soapenv',
-		'http://schemas.xmlsoap.org/soap/encoding/' => 'soap',
-		'http://www.w3.org/2001/XMLSchema' => 'xsd',
-		'http://www.w3.org/2001/XMLSchema-instance' => 'xsi',
-		'urn:dslforum-org:cwmp-1-0' => '',
-	},
-	rules => [
-		#_default => 'content trim',
-		x_default => sub {
-			my ($tag_name, $tag_hash, $context, $parent_data) = @_;
-			warn dump( $tag_name, $tag_hash, $context );
-		},
-		'ID' => sub {
-			my ($tag_name, $tag_hash, $context, $parent_data) = @_;
-			$state->{ID} = $tag_hash->{_content};
-		},
-
-=head2 Inform
-
-Generate InformResponse to CPE
-
-=cut
-
-		'Inform' => sub {
-			$state->{_dispatch} = 'InformResponse';		# what reponse to call
-		},
-		'DeviceId' => sub {
-			my ($tag_name, $tag_hash, $context, $parent_data) = @_;
-			foreach my $name ( keys %$tag_hash ) {
-				next if $name eq '_content';
-				my $key = $name;
-				$key =~ s/^\w+://;	# stip namespace
-				$state->{DeviceID}->{ $key } = _tag( $tag_hash, $name, '_content' );
-			}
-		},
-		'EventStruct' => sub {
-			my ($tag_name, $tag_hash, $context, $parent_data) = @_;
-			push @{ $state->{EventStruct} }, $tag_hash->{EventCode}->{_content};
-		},
-		qr/(MaxEnvelopes|CurrentTime|RetryCount)/ => sub {
-			my ($tag_name, $tag_hash, $context, $parent_data) = @_;
-			$state->{$tag_name} = $tag_hash->{_content};
-		},
-		'ParameterValueStruct' => sub {
-			my ($tag_name, $tag_hash, $context, $parent_data) = @_;
-			# Name/Value tags must be case insnesitive
-			my $value = (grep( /value/i, keys %$tag_hash ))[0];
-			$state->{Parameter}->{ _tag($tag_hash, 'Name', '_content') } = _tag($tag_hash, 'Value', '_content' );
-		},
-
-=head2 GetRPCMethodsResponse
-
-=cut
-		qr/^(?:^\w+:)*string$/ => 'content array',
-		'MethodList' => sub {
-			my ($tag_name, $tag_hash, $context, $parent_data) = @_;
-			$state->{MethodList} = _tag( $tag_hash, 'string' );
-		},
-
-=head2 GetParameterNamesResponse
-
-=cut
-
-		'ParameterInfoStruct' => sub {
-			my ($tag_name, $tag_hash, $context, $parent_data) = @_;
-			my $name = _tag($tag_hash, 'Name', '_content');
-			my $writable = _tag($tag_hash, 'Writable', '_content' );
-
-			confess "need state" unless ( $state );	# don't remove!
-
-			# XXX dragons ahead: convert name to tree rewriting it into perl
-			my $s = '$state->{ParameterInfo}->' . $tree->name2perl( $name ) . "->{writable} = $writable;";
-			eval "$s";
-			confess "can't eval $s : $@" if ($@);
-
-			#warn "## state = dump( $state ), "\n";
-		},
-	
-=head2 Fault
-
-=cut
-
-		'Fault' => sub {
-			my ($tag_name, $tag_hash, $context, $parent_data) = @_;
-			$state->{Fault} = {
-				FaultCode => _tag( $tag_hash, 'FaultCode', '_content' ),
-				FaultString => _tag( $tag_hash, 'FaultString', '_content' ),
-			};
-			warn "FAULT: ", $state->{Fault}->{FaultCode}, " ", $state->{Fault}->{FaultString}, "\n";
-		}
-	]
-);
 
 1;
