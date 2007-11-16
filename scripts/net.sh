71970361377c34791fd6e7970a32fd2ba345d8bc
Dobrica Pavlinusic <dpavlin@rot13.org>
2007-11-16 18:33:04 +0000

    r226@brr:  dpavlin | 2007-11-16 19:32:44 +0100  change addressing
    git-svn-id: https://perl-cwmp.googlecode.com/svn/trunk@206 836a5e1a-633d-0410-964b-294494ad4392

--- scripts/net.sh
+++ scripts/net.sh
@@ -11,6 +11,6 @@ fi
 sudo ifconfig $eth down
 sudo ifconfig $eth 192.168.1.241 up
 # backup network (thompson 10.0.0.138)
-sudo ifconfig $eth:10 10.0.0.1 up
+sudo ifconfig $eth:10 10.0.0.100 up
 sudo route add -net 192.168.1.240/28 dev $eth
 echo "Setup of $eth to 192.168.1.90 ready"
