5c3938cea5f3b2ae4a4413f9ab6811e9e8cd1bb8
Dobrica Pavlinusic <dpavlin@rot13.org>
2010-03-12 20:13:36 +0000

    install perl modules packaged in Debian

---
+++ bin/install-debian.sh
@@ -0,0 +1,5 @@
+#!/bin/sh -x
+
+sudo apt-get install libmodule-install-perl libxml-generator-perl libdata-dump-perl \
+	libclass-accessor-perl libfile-slurp-perl libhash-merge-perl libyaml-syck-perl \
+	libyaml-perl
