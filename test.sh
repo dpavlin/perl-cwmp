3a80b20407192fd06423c58a8eae7936bffae2a6
samplonius <tom@samplonius.org>
2011-05-13 12:44:53 -0700

    Add some files that test.sh was referring to, and patch test.sh so it works a bit better. GetRPCMethods appears to not work though.

--- test.sh
+++ test.sh
@@ -1,5 +1,7 @@
+#! /bin/bash
+
 POST -c 'text/xml' http://localhost:3333 < protocol/inform.xml
-echo -e "\n\n";
-echo | POST -c 'text/xml' http://localhost:3333 
-echo -e "\n\n";
-POST -c 'text/xml' http://localhost:3333 < protocol/thompson-GetRPCMethodsResponse
+echo -e "\n\n"
+#echo | POST -c 'text/xml' http://localhost:3333 
+#echo -e "\n\n"
+POST -c 'text/xml' http://localhost:3333 < protocol/getrpcmethods.xml
