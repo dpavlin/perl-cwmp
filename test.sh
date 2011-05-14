3bdc10f62aedbf84ce4a76615d3435e8e52cd278
samplonius <tom@samplonius.org>
2011-05-13 17:40:11 -0700

    Dump response headers

--- test.sh
+++ test.sh
@@ -1,7 +1,5 @@
 #! /bin/bash
 
-POST -c 'text/xml' http://localhost:3333 < protocol/inform.xml
+POST -ec 'text/xml' http://localhost:3333 < protocol/inform.xml
 echo -e "\n\n"
-#echo | POST -c 'text/xml' http://localhost:3333 
-#echo -e "\n\n"
-POST -c 'text/xml' http://localhost:3333 < protocol/getrpcmethods.xml
+#POST -c 'text/xml' http://localhost:3333 < protocol/getrpcmethods.xml
