dad50b8bf969069a449a21b309b06cd07b4da9eb
Dobrica Pavlinusic <dpavlin@rot13.org>
2012-12-06 00:37:09 +0100

    install or build all dependencies #2

--- bin/install-debian.sh
+++ bin/install-debian.sh
@@ -1,5 +1,11 @@
 #!/bin/sh -x
 
-sudo apt-get install libmodule-install-perl libxml-generator-perl libdata-dump-perl \
+sudo apt-get install libmodule-install-perl libdata-dump-perl \
 	libclass-accessor-perl libfile-slurp-perl libhash-merge-perl libyaml-syck-perl \
-	libyaml-perl build-essential
+	libyaml-perl build-essential \
+	libtest-simple-perl libtest-expect-perl libnet-telnet-perl \
+	libpod-xhtml-perl libtest-pod-coverage-perl \
+	libxml-bare-perl libpod-xhtml-perl libwww-mechanize-perl
+
+test -f libipc-dirqueue-perl*.deb || cpan2deb IPC::DirQueue
+sudo dpkg -i libipc-dirqueue-perl*.deb
