3d0c2bfebe88a524127f1a4a464ef10a61c5b925
Dobrica Pavlinusic <dpavlin@rot13.org>
2012-12-10 20:21:16 +0100

    two additional dependencies on clean installation

--- bin/install-debian.sh
+++ bin/install-debian.sh
@@ -5,7 +5,8 @@ sudo apt-get install libmodule-install-perl libdata-dump-perl \
 	libyaml-perl build-essential \
 	libtest-simple-perl libtest-expect-perl libnet-telnet-perl \
 	libpod-xhtml-perl libtest-pod-coverage-perl \
-	libxml-bare-perl libpod-xhtml-perl libwww-mechanize-perl
+	libxml-bare-perl libpod-xhtml-perl libwww-mechanize-perl \
+	libxml-generator-perl dh-make-perl
 
 test -f libipc-dirqueue-perl*.deb || cpan2deb IPC::DirQueue
 sudo dpkg -i libipc-dirqueue-perl*.deb
